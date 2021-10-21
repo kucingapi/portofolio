ea65ef8424474d0dc72d374e42bfc33577278b9a
novel <novelbafagih@student.ub.ac.id>
2021-10-21 09:06:59 +0700

    navbar is now responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Contacts, Home, Person, Settings, Work } from "@mui/icons-material"
-import { AppBar,  Box,  Tab,  Tabs,  Toolbar} from "@mui/material"
+import { AppBar, Tab, Tabs, Toolbar } from "@mui/material"
 import { makeStyles } from "@mui/styles";
 import { useState } from "react";
 
@@ -8,12 +8,6 @@ const useStyles = makeStyles((theme) => ({
 		display:"flex",
 		alignSelf:"center"
 	},
-	item:{
-		[theme.breakpoints.up('md')]:{
-			paddingLeft:"2.5vw",
-			paddingRight:"2.5vw",
-		},
-	}
 }));
 
 export const Navbar = () =>{
@@ -27,19 +21,34 @@ export const Navbar = () =>{
 	return (
 		<AppBar>
 			<Toolbar className={classes.toolbar}>
-				<Tabs 
-					value={value} 
-					onChange={handleChange} 
-					textColor="secondary" 
-					indicatorColor="secondary"
-					centered
-				>
-					<Tab icon={<Home fontSize="large" className={classes.item}/>}/>
-					<Tab icon={<Person fontSize="large" className={classes.item}/>}/>
-					<Tab icon={<Settings fontSize="large" className={classes.item}/>}/>
-					<Tab icon={<Work fontSize="large" className={classes.item}/>}/>
-					<Tab icon={<Contacts fontSize="large" className={classes.item}/>}/>
-				</Tabs>
+					<Tabs 
+						value={value} 
+						onChange={handleChange} 
+						textColor="secondary" 
+						indicatorColor="secondary"
+						
+					>
+						<Tab 
+							sx={{minWidth:{sm:"0px",xs:"0px",md:"90px"}}} 
+							icon={<Home fontSize="large" />}
+						/>
+						<Tab 
+							sx={{minWidth:{sm:"0px",xs:"0px",md:"90px"}}} 
+							icon={<Person fontSize="large" />}
+						/>
+						<Tab 
+							sx={{minWidth:{sm:"0px",xs:"0px",md:"90px"}}} 
+							icon={<Settings fontSize="large" />}
+						/>
+						<Tab 
+							sx={{minWidth:{sm:"0px",xs:"0px",md:"90px"}}} 
+							icon={<Work fontSize="large" />}
+						/>
+						<Tab 
+							sx={{minWidth:{sm:"0px",xs:"0px",md:"90px"}}} 
+							icon={<Contacts fontSize="large" />}
+						/>
+					</Tabs>
 			</Toolbar>
 		</AppBar>
 	)
